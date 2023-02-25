ec29535b28a20bddf30f689fe66252a80a73e9b5
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-24 21:28:43 -0800

    Begins file structure for components and starts working on index.js and App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import PortfolioContainer from "./components/PortfolioContainer";
+
+function App() {
+  return (
+    <div className="">
+      <PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
