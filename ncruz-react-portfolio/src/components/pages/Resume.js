c6768e90dceba859bfffb5a00c7857d2f744d8f1
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-26 21:13:12 -0800

    Starts resume component

---
+++ ncruz-react-portfolio/src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div className="resume">
+      <h1>Resume</h1>
+      <div>
+        <p>Front-End</p>
+        <p>Back-End</p>
+        <p>Additional Skills</p>
+      </div>
+    </div>
+  );
+}
