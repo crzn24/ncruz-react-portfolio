24640110d6b059532c7767349e2823db034b4ca5
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-27 12:54:31 -0800

    Adds padding

--- ncruz-react-portfolio/src/components/pages/Resume.js
+++ ncruz-react-portfolio/src/components/pages/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
   return (
     <section className="bg-dark">
       <div className="container">
-        <div className="col-lg-10 mx-auto text-white mb-4">
+        <div className="col-lg-10 mx-auto text-white mb-4 pt-4">
           <h1 className="text-center">Resume</h1>
           <hr className="light my-4" />
           <h3>Front-End</h3>
