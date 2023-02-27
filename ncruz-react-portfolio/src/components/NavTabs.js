c055668bbe38c2bb206b68595bc4699e69f3172b
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-26 21:25:33 -0800

    Adds name to nav tabs header

--- ncruz-react-portfolio/src/components/NavTabs.js
+++ ncruz-react-portfolio/src/components/NavTabs.js
@@ -3,9 +3,10 @@ import React from "react";
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
-    return (
+  return (
+    <div>
+      <h1>Nathan Cruz</h1>
       <ul className="nav nav-tabs">
-
         <li className="nav-item">
           <a
             href="#aboutme"
@@ -58,7 +59,8 @@ function NavTabs({ currentPage, handlePageChange }) {
           </a>
         </li>
       </ul>
-    );
+    </div>
+  );
 }
 
 export default NavTabs;
