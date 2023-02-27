0c9878cfbb399bca5f5d06637dfb322f24ffa723
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-27 13:47:44 -0800

    Creates img element for profile pic and adds import of png file

--- ncruz-react-portfolio/src/components/pages/AboutMe.js
+++ ncruz-react-portfolio/src/components/pages/AboutMe.js
@@ -1,20 +1,29 @@
 import React from "react";
+import profilePhoto from "../../assets/images/ncruz-photo.PNG";
 
 export default function AboutMe() {
   return (
     <div>
-      <div className="aboutMe">
-        <h1 className="text-center">About Me</h1>
-        <p>
-          I am an elementary school educator that mainly works with students
-          with special needs. I graduated from the University of Washington in
-          2014 with a BA degree in American Ethnic Studies and minors in
-          Diversity and Education, Learning, & Society. I am currently enrolled
-          in a full stack web developer bootcamp at the University of
-          Washington. I enjoy sports, fitness, hiking, Star Wars, gaming, and
-          all forms of art and creative expression. I also enjoy solving
-          problems by thinking outside the box.
-        </p>
+      <div className="aboutMe d-flex text-center">
+        <div className="container my-auto">
+          <div className="row">
+            <div className="col-lg-10 mx-auto">
+              <h1 className="text-center">About Me</h1>
+              <hr />
+              <p className="mb-5">
+                I am an elementary school educator that mainly works with
+                students with special needs. I graduated from the University of
+                Washington in 2014 with a BA degree in American Ethnic Studies
+                and minors in Diversity and Education, Learning, & Society. I am
+                currently enrolled in a full stack web developer bootcamp at the
+                University of Washington. I enjoy sports, fitness, hiking, Star
+                Wars, gaming, and all forms of art and creative expression. I
+                also enjoy solving problems by thinking outside the box.
+              </p>
+              <img className="circular_image" src={profilePhoto}></img>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
