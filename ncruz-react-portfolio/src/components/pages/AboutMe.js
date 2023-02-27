0657c4e77fa5788a5e6a37660519656bda7b576d
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-26 21:42:29 -0800

    Works on some initial styling and get contact form to kind of work

--- ncruz-react-portfolio/src/components/pages/AboutMe.js
+++ ncruz-react-portfolio/src/components/pages/AboutMe.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutMe() {
   return (
     <div className="aboutMe">
-      <h1>About Me</h1>
+      <h1 className="text-center">About Me</h1>
       <p>
       I am an elementary school educator that mainly works with students with special needs. I graduated from the University of Washington in 2014 with a BA degree in American Ethnic Studies and minors in Diversity and Education, Learning, & Society. I am currently enrolled in a full stack web developer bootcamp at the University of Washington. I enjoy sports, fitness, hiking, Star Wars, gaming, and all forms of art and creative expression. I also enjoy solving problems by thinking outside the box.
       </p>
--- ncruz-react-portfolio/src/components/pages/Resume.js
+++ ncruz-react-portfolio/src/components/pages/Resume.js
@@ -2,13 +2,28 @@ import React from "react";
 
 export default function Resume() {
   return (
-    <div className="resume">
-      <h1>Resume</h1>
-      <div>
-        <p>Front-End</p>
-        <p>Back-End</p>
-        <p>Additional Skills</p>
+    <section className="bg-dark">
+      <div className="container">
+        <div className="col-lg-10 mx-auto text-white mb-4">
+          <h1 className="text-center">Resume</h1>
+          <hr className="light my-4" />
+          <h3>Front-End</h3>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+          <h3>Back-End</h3>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+          <h3>Additional Skills</h3>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
