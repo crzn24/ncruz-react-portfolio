391b33a9e8742ebcb36ed33bf9005c19030be532
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-26 20:03:36 -0800

    Deletes reference to home in method and comments out Home import

--- ncruz-react-portfolio/src/components/PortfolioContainer.js
+++ ncruz-react-portfolio/src/components/PortfolioContainer.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import NavTabs from "./NavTabs";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -8,15 +8,12 @@ import Resume from "./pages/Resume";
 import Footer from "./Footer";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState("Home");
+  const [currentPage, setCurrentPage] = useState("AboutMe");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === "Home") {
-      return <Home />;
-    }
     if (currentPage === "AboutMe") {
-      return <AboutMe />; //AboutMe?
+      return <AboutMe />; //About?
     }
     if (currentPage === "Portfolio") {
       return <Portfolio />;
