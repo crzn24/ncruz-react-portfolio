49092f623038c8d5716afa866465cd7e8e8f2e1c
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-26 19:29:28 -0800

    Creates a header.js without navbar styling

---
+++ ncruz-react-portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./NavTabs";
+
+// TODO: DO NAVBAR
+export default function Header() {
+  return (
+    <header className="header">
+      <div>
+        <Navigation />
+      </div>
+    </header>
+  );
+}
