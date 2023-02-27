305e457b0527ca0b5d88f0e9e21a7c484cf4cf21
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-26 19:54:33 -0800

    Npm i react-icons, edits footer to show icons, edits portfolio container to show footer component

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ncruz-react-portfolio
+
+React Icons for Footer
+https://react-icons.github.io/react-icons/
---
+++ ncruz-react-portfolio/src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+// ICONS FOR FOOTER
+import {
+  // GitHub:
+  AiFillGithub,
+  // LinkedIn:
+  AiFillLinkedin,
+} from "react-icons/ai";
+
+import {
+  // Stack Overflow
+  BsStackOverflow,
+} from "react-icons/bs";
+
+
+// TODO: FIGURE OUT LINK TO RESUME
+
+export default function Footer() {
+  return (
+    <div id="footer-contact">
+      <div className="contact-icons">
+        <a href="https://github.com/crzn24" target="_blank" rel="noreferrer">
+          <AiFillGithub />
+        </a>
+      </div>
+
+      <div className="contact-icons">
+        <a
+          href="https://www.linkedin.com/in/nathancruz-/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillLinkedin />
+        </a>
+      </div>
+
+      <div className="contact-icons">
+        <a
+          href="https://stackexchange.com/users/27874497/crzn24"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsStackOverflow />
+        </a>
+      </div>
+    </div>
+  );
+}
