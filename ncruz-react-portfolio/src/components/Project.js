90814080133de43ce28df2983b712cef0d7d1c2e
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-27 18:11:42 -0800

    Starts to figure out how images work for project component

--- ncruz-react-portfolio/src/components/Project.js
+++ ncruz-react-portfolio/src/components/Project.js
@@ -1,23 +1,26 @@
 import React from "react";
 
-export default function Project({project}) {
-    const {name, description, repo, link,  } = project;
+export default function Project({ project }) {
+  const { name, description, repo, link } = project;
 
-    return (
-      <div className="">
-        <image
+  return (
+    <div className="col-lg-4 col-sm-6">
+      <div className="portfolio-box">
+        {/* <image
             src={require(`../../assets/images/portfolio/${name}.jpg`)}
             className=""
-        />
-        
-        <div>
+        /> */}
+
+        <div className="portfolio-box-caption">
+          <div className="portfolio-box-caption-content">
             <a href={repo}>Repository</a>
             <a href={link}>Deployed App</a>
             <p className="">{description}</p>
+          </div>
         </div>
-        
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 //   TODO: FIGURE OUT PROJECT COMPONENT
