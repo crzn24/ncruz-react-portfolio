6e811d113d0ece4e6e7b65f6e2703e0b10c77ed6
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-03-01 20:03:52 -0800

    Comments out other projects to figure out individual project displaying

--- ncruz-react-portfolio/src/components/Project.js
+++ ncruz-react-portfolio/src/components/Project.js
@@ -1,5 +1,10 @@
 import React from "react";
 // import Project1 from "../assets/images/steam-achievement-picker.jpeg" // TODO: Do i need to import each picture individually or is there an easier way?
+import {
+  // GitHub:
+  AiFillGithub
+} from "react-icons/ai";
+
 export default function Project({ project }) {
   const { name, description, repo, link } = project;
 
@@ -15,8 +20,8 @@ export default function Project({ project }) {
 
         <div className="portfolio-box-caption">
           <div className="portfolio-box-caption-content">
-            <a href={repo} className="project-category text-faded"><i className=""></i></a>
-            <a href={link} className="project-category text-faded">{name}</a>
+            <a href={repo} className="project-category text-faded"><i className=""><AiFillGithub /></i></a>
+            <a href={link} className="project-category text-faded text-decoration">{name}</a>
             <p className="project-name">{description}</p>
           </div>
         </div>
